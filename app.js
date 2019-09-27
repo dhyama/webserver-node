c246e0134acbfaffee46cb0fc123224c0d7f357c
Miguel Angel <dhyama@gmail.com>
2019-09-27 12:34:20 +0200

    Aplicación funcional

--- app.js
+++ app.js
@@ -11,5 +11,23 @@ const http = require('http');
  * res -> parametros de salida
  */
 http.createServer((req, res) => {
-    res.write('Hola Mundo');
-})
+
+    //Creamos la cabecera de tipo json
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    //Creamos la salida json que queremos devolver
+    let salida = {
+        nombre: 'Miguel Ángel',
+        edad: 42,
+        url: req.url
+    }
+
+    //Damos formato json a la salida
+    res.write(JSON.stringify(salida));
+    // res.write('Hola Mundo');
+    //Finalizamos la respuesta del servicio
+    res.end();
+}).listen(8080);
+
+//Salida para indicar que el servicio esta en funcionamiento.
+console.log('Escuchando en el puesto 8080');
